8e796a2ab20c0068451207b21f07d6a839efe63c
7006 <22399553+7006@users.noreply.github.com>
2023-08-26 22:24:59 +0300

    Update lesson3_task04.erl

--- lesson3_task04.erl
+++ lesson3_task04.erl
@@ -1,7 +1,8 @@
 -module(lesson3_task04).
 -export([decode/2]).
 
-decode(JsonText, proplist) ->
+% Написати парсер jSON (має вміти працювати і з map і з proplists)
+decode(JsonText, proplists) ->
     ok;
 decode(JsonText, map) ->
     ok.
