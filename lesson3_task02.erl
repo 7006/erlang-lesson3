740b88d47d835971f7bd0c3a30ada49ce612c735
7006 <22399553+7006@users.noreply.github.com>
2023-08-29 21:18:26 +0000

    Add words/1 implementation

--- lesson3_task02.erl
+++ lesson3_task02.erl
@@ -1,6 +1,30 @@
 -module(lesson3_task02).
 -export([words/1]).
 
+-define(is_space(Char), (Char =:= 32)).
+
 % Розділити рядок на слова
-words(BinText) ->
-    ok.
+words(Text) ->
+    words(Text, [<<>>]).
+
+words(
+    <<Char/utf8, NextChar/utf8, Text/binary>>,
+    Words
+) when
+    ?is_space(Char),
+    not ?is_space(NextChar)
+->
+    words(
+        Text,
+        [<<NextChar/utf8>> | Words]
+    );
+words(
+    <<Char/utf8, Text/binary>>,
+    [<<Word/binary>> | Words]
+) ->
+    words(
+        Text,
+        [<<Word/binary, Char/utf8>> | Words]
+    );
+words(<<>>, Words) ->
+    lists:reverse(Words).
