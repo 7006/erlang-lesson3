16676c110f22a105fb2016363963e8bfee81beb5
7006 <22399553+7006@users.noreply.github.com>
2023-08-29 19:09:39 +0000

    Add first_word/1 implementation

--- lesson3_task01.erl
+++ lesson3_task01.erl
@@ -2,5 +2,14 @@
 -export([first_word/1]).
 
 % Витягти з рядка перше слово
-first_word(BinText) ->
-    ok.
+-define(is_space(Char), (Char =:= 32)).
+
+first_word(<<Char/utf8, NextChar/utf8, _/binary>>) when
+    not ?is_space(Char),
+    ?is_space(NextChar)
+->
+    <<Char/utf8>>;
+first_word(<<Char/utf8, Text/binary>>) ->
+    <<Char/utf8, (first_word(Text))/binary>>;
+first_word(<<>>) ->
+    <<>>.
--- lesson3_task01_tests.erl
+++ lesson3_task01_tests.erl
@@ -6,10 +6,38 @@
 first_word_test_() ->
     [
         {
-            "it should get a first word",
+            "it should get a first word for ascii text",
             ?_assertEqual(
                 <<"Some">>,
                 first_word(<<"Some Text">>)
+            )
+        },
+        {
+            "it should get a first word for utf8 text",
+            ?_assertEqual(
+                <<"Якийсь">>,
+                first_word(<<"Якийсь текст">>)
+            )
+        },
+        {
+            "it should get a first word for ascii text with spaces at the beginning",
+            ?_assertEqual(
+                <<"    Some">>,
+                first_word(<<"    Some Text">>)
+            )
+        },
+        {
+            "it should get a first word for utf8 text with spaces at the beginning",
+            ?_assertEqual(
+                <<"    Якийсь">>,
+                first_word(<<"    Якийсь текст">>)
+            )
+        },
+        {
+            "it should get a empty string for an empty string",
+            ?_assertEqual(
+                <<"">>,
+                first_word(<<"">>)
             )
         }
     ].
