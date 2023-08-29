5d13b3a1cab87508870df44b393d74f3d47758d0
7006 <22399553+7006@users.noreply.github.com>
2023-08-29 19:28:29 +0000

    Add failed tests for words/1

--- lesson3_task02_tests.erl
+++ lesson3_task02_tests.erl
@@ -6,10 +6,38 @@
 words_test_() ->
     [
         {
-            "it should split a text to the words using a space character as a separator",
+            "it should split ascii text to the words using a space character as a separator",
             ?_assertEqual(
                 [<<"Text">>, <<"with">>, <<"four">>, <<"words">>],
                 words(<<"Text with four words">>)
+            )
+        },
+        {
+            "it should split utf8 text to the words using a space character as a separator",
+            ?_assertEqual(
+                [<<"Текст"/utf8>>, <<"з"/utf8>>, <<"чотирьох"/utf8>>, <<"слів"/utf8>>],
+                words(<<"Текст з чотирьох слів"/utf8>>)
+            )
+        },
+        {
+            "it should get a one-element list for one-word ascii text using a space character as a separator",
+            ?_assertEqual(
+                [<<"Text">>],
+                words(<<"Text">>)
+            )
+        },
+        {
+            "it should get a one-element list for one-word utf8 text using a space character as a separator",
+            ?_assertEqual(
+                [<<"Текст"/utf8>>],
+                words(<<"Текст"/utf8>>)
+            )
+        },
+        {
+            "it should get a one-element list with empty string for an empty string",
+            ?_assertEqual(
+                [<<"">>],
+                words(<<"">>)
             )
         }
     ].
