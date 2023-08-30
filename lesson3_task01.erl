2d4b24b217f6514706f933e9ebc50ca14113a2bb
7006 <22399553+7006@users.noreply.github.com>
2023-08-30 04:08:15 +0000

    rewrite first_word/1 using case of

--- lesson3_task01.erl
+++ lesson3_task01.erl
@@ -4,12 +4,15 @@
 -define(is_space(Char), (Char =:= 32)).
 
 % Витягти з рядка перше слово
-first_word(<<Char/utf8, NextChar/utf8, _/binary>>) when
-    not ?is_space(Char),
-    ?is_space(NextChar)
-->
-    <<Char/utf8>>;
-first_word(<<Char/utf8, Text/binary>>) ->
-    <<Char/utf8, (first_word(Text))/binary>>;
-first_word(<<>>) ->
-    <<>>.
+first_word(Text) ->
+    case Text of
+        <<PrevChar/utf8, Char/utf8, _/binary>> when
+            not ?is_space(PrevChar), ?is_space(Char)
+        ->
+            <<PrevChar/utf8>>;
+        <<Char/utf8, RestText/binary>> ->
+            NextText = first_word(RestText),
+            <<Char/utf8, NextText/binary>>;
+        <<>> ->
+            <<>>
+    end.
