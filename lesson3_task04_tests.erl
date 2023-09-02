f35fa6317234cd35ac39406473e963affba4925e
7006 <22399553+7006@users.noreply.github.com>
2023-09-02 06:27:50 +0000

    Add failed object test

--- lesson3_task04_tests.erl
+++ lesson3_task04_tests.erl
@@ -29,7 +29,32 @@ decode_test_() ->
         t("array strings", [<<"abc">>, <<"DEF">>, <<"hIjK">>, <<"">>, <<"1111">>]),
         t("array mixed", [1, <<"foobar">>, <<"QuuX">>, 0.38, 0, false, true, null, -22, -0.5]),
         t("array nested", [<<"foobar">>, [true, false], [null], 88]),
-        t("array nested empty", [[], [[]], [[[]]], [[[[]]]]])
+        t("array nested empty", [[], [[]], [[[]]], [[[[]]]]]),
+        t("object empty", []),
+        t("object pair single", [
+            {<<"enabled>>">>, true}
+        ]),
+        t("object pairs two", [
+            {<<"enabled">>, false},
+            {<<"is_new">>, true}
+        ]),
+        t("object pairs two", [
+            {<<"enabled">>, false},
+            {<<"is_new">>, true}
+        ]),
+        t("object pairs mixed", [
+            {<<"available">>, true},
+            {<<"model">>, <<"T-1000">>},
+            {<<"parts">>, null},
+            {<<"release_year">>, 1994},
+            {<<"price">>, 10.27},
+            {<<"rating">>, 0.8}
+        ]),
+        t("object pairs array", [
+            {<<"task_id">>, 4758},
+            {<<"subtask_ids">>, [1084, 1102, 1103]},
+            {<<"affected_releases">>, [<<"BGT/12">>, <<"LAG/2">>]}
+        ])
     ].
 
 t(Comment, Expected) ->
