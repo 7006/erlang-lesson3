d479e0f04df564468cf519220cc18fddef1eeba2
7006 <22399553+7006@users.noreply.github.com>
2023-09-02 23:45:42 +0000

    Make tail recursive

--- lesson3_task01.erl
+++ lesson3_task01.erl
@@ -4,11 +4,14 @@
 
 % Витягти з рядка перше слово
 first_word(Text) ->
+    first_word(Text, <<>>).
+
+first_word(Text, Word) ->
     case Text of
         <<Character/utf8, $\s, _/binary>> when Character =/= $\s ->
-            <<Character/utf8>>;
+            <<Word/binary, Character/utf8>>;
         <<Character/utf8, RestText/binary>> ->
-            <<Character/utf8, (first_word(RestText))/binary>>;
+            first_word(RestText, <<Word/binary, Character/utf8>>);
         <<>> ->
-            <<>>
+            Word
     end.
