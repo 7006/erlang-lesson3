081cac3db8ca93e3589afc148ce96da5e8d5ea2c
7006 <22399553+7006@users.noreply.github.com>
2023-09-03 00:19:43 +0000

    Do pattern matching in function args

--- lesson3_task02.erl
+++ lesson3_task02.erl
@@ -6,12 +6,11 @@
 words(Text) ->
     words(Text, [<<>>]).
 
-words(Text, Words) ->
+words(Text, [<<Word/binary>> | RestWords] = Words) ->
     case Text of
         <<$\s, Char/utf8, RestText/binary>> when Char =/= $\s ->
             words(RestText, [<<Char/utf8>> | Words]);
         <<Char/utf8, RestText/binary>> ->
-            [<<Word/binary>> | RestWords] = Words,
             words(RestText, [<<Word/binary, Char/utf8>> | RestWords]);
         <<>> ->
             reverse(Words)
