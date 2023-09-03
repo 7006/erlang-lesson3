e25e35e3cb4cbb31c1769f21186f839c522bc739
7006 <22399553+7006@users.noreply.github.com>
2023-09-03 23:11:24 +0000

    Simplify words/1

--- lesson3_task02.erl
+++ lesson3_task02.erl
@@ -6,18 +6,16 @@
 words(Text) ->
     words(Text, <<>>, []).
 
-words(<<>>, <<>>, Words) ->
-    lesson3_lists:reverse(Words);
-words(<<>>, Word, Words) ->
-    words(<<>>, <<>>, [Word | Words]);
 words(Text, <<Word/binary>>, Words) ->
     case Text of
         <<$\s, RestText/binary>> ->
             words(RestText, Word, Words);
         <<Char/utf8, $\s, RestText/binary>> ->
             words(RestText, <<>>, [<<Word/binary, Char/utf8>> | Words]);
+        <<Char/utf8>> ->
+            words(<<>>, <<>>, [<<Word/binary, Char/utf8>> | Words]);
         <<Char/utf8, RestText/binary>> ->
             words(RestText, <<Word/binary, Char/utf8>>, Words);
         <<>> ->
-            words(<<>>, Word, Words)
+            lesson3_lists:reverse(Words)
     end.
