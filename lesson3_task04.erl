05b60a25dbb3eea0070bcd5d80e28defd134a138
Replit user <>
2023-09-05 13:50:06 +0000

    cleanup

--- lesson3_task04.erl
+++ lesson3_task04.erl
@@ -18,41 +18,20 @@ decode(Text) ->
             {Array, <<>>} = decode_array(RestText),
             Array
     end.
+
 %% ----------------------------------------------------------------------------
 %% decode_object
 %% ----------------------------------------------------------------------------
 decode_object(Text) ->
     decode_object(Text, [], no_key, no_value).
 
-% { "a" : 1, "b" : [true], "c": ["aaaa"] }
 decode_object(Text, Object, Key1, Value1) ->
     case get_token(Text) of
         {value, Key, RestText} when Key1 =:= no_key, Value1 =:= no_value ->
-            io:format(user, "~n~n1 ~p ~p ~p ~p ~p~n", [
-                Key,
-                RestText,
-                Key1,
-                Value1,
-                Object
-            ]),
             decode_object(RestText, Object, Key, no_value);
         {colon, RestText} when Key1 =/= no_key ->
-            io:format(user, "~n~ncolon ~p ~p ~p ~p ~p~n", [
-                xxx,
-                RestText,
-                Key1,
-                Value1,
-                Object
-            ]),
             decode_object(RestText, Object, Key1, no_value);
         {value, Value, RestText} when Key1 =/= no_key, Value1 =:= no_value ->
-            io:format(user, "~n~n3 ~p ~p ~p ~p ~p~n", [
-                Value,
-                RestText,
-                Key1,
-                Value1,
-                Object
-            ]),
             decode_object(RestText, Object, Key1, Value);
         {enter_array, RestText} when Key1 =/= no_key, Value1 =:= no_value ->
             {Array, NextText} = decode_array(RestText),
@@ -61,27 +40,13 @@ decode_object(Text, Object, Key1, Value1) ->
             {NestedObject, NextText} = decode_object(RestText),
             decode_object(NextText, Object, Key1, NestedObject);
         {comma, RestText} when Key1 =/= no_key, Value1 =/= no_value ->
-            % M1 = maps:put(Key1, Value1, M),
             Object1 = [{Key1, Value1} | Object],
             decode_object(RestText, Object1, no_key, no_value);
         {exit_object, RestText} when Key1 =:= no_key, Value1 =:= no_value ->
             {Object, RestText};
         {exit_object, RestText} when Key1 =/= no_key, Value1 =/= no_value ->
-            io:format(user, "~n~nexit object ~p ~p ~p ~p ~p~n", [
-                yyy,
-                RestText,
-                Key1,
-                Value1,
-                Object
-            ]),
             Object1 = [{Key1, Value1} | Object],
             Object2 = lists:reverse(Object1),
-
-            io:format(user, "~n~nexit object ~p ~p~n", [
-                RestText,
-                Object2
-            ]),
-
             {Object2, RestText}
     end.
 
