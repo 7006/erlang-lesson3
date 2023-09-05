41dcdb918927d267642874cfc89d1b1933e798d4
Replit user <unknown>
2023-09-05 12:40:22 +0000

    decode object with nested arrays

--- lesson3_task04.erl
+++ lesson3_task04.erl
@@ -11,9 +11,75 @@ decode(Text) ->
     case get_token(Text) of
         {value, V, <<>>} ->
             V;
+        {enter_object, RestText} ->
+            {Object, <<>>} = decode_object(RestText),
+            Object;
         {enter_array, RestText} ->
             {Array, <<>>} = decode_array(RestText),
             Array
+    end.
+%% ----------------------------------------------------------------------------
+%% decode_object
+%% ----------------------------------------------------------------------------
+decode_object(Text) ->
+    decode_object(Text, [], no_key, no_value).
+
+% { "a" : 1, "b" : [true], "c": ["aaaa"] }
+decode_object(Text, Object, Key1, Value1) ->
+    case get_token(Text) of
+        {value, Key, RestText} when Key1 =:= no_key, Value1 =:= no_value ->
+            io:format(user, "~n~n1 ~p ~p ~p ~p ~p~n", [
+                Key,
+                RestText,
+                Key1,
+                Value1,
+                Object
+            ]),
+            decode_object(RestText, Object, Key, no_value);
+        {colon, RestText} when Key1 =/= no_key ->
+            io:format(user, "~n~ncolon ~p ~p ~p ~p ~p~n", [
+                xxx,
+                RestText,
+                Key1,
+                Value1,
+                Object
+            ]),
+            decode_object(RestText, Object, Key1, no_value);
+        {value, Value, RestText} when Key1 =/= no_key, Value1 =:= no_value ->
+            io:format(user, "~n~n3 ~p ~p ~p ~p ~p~n", [
+                Value,
+                RestText,
+                Key1,
+                Value1,
+                Object
+            ]),
+            decode_object(RestText, Object, Key1, Value);
+        {enter_array, RestText} when Key1 =/= no_key, Value1 =:= no_value ->
+            {Array, NextText} = decode_array(RestText),
+            decode_object(NextText, Object, Key1, Array);
+        {comma, RestText} when Key1 =/= no_key, Value1 =/= no_value ->
+            % M1 = maps:put(Key1, Value1, M),
+            Object1 = [{Key1, Value1} | Object],
+            decode_object(RestText, Object1, no_key, no_value);
+        {exit_object, RestText} when Key1 =:= no_key, Value1 =:= no_value ->
+            {Object, RestText};
+        {exit_object, RestText} when Key1 =/= no_key, Value1 =/= no_value ->
+            io:format(user, "~n~nexit object ~p ~p ~p ~p ~p~n", [
+                yyy,
+                RestText,
+                Key1,
+                Value1,
+                Object
+            ]),
+            Object1 = [{Key1, Value1} | Object],
+            Object2 = lists:reverse(Object1),
+
+            io:format(user, "~n~nexit object ~p ~p~n", [
+                RestText,
+                Object2
+            ]),
+
+            {Object2, RestText}
     end.
 
 %% ----------------------------------------------------------------------------
@@ -42,10 +108,16 @@ get_token(Text) ->
     case Text of
         <<C, RestText/binary>> when ?is_whitespace(C) ->
             get_token(RestText);
+        <<"{", RestText/binary>> ->
+            {enter_object, RestText};
+        <<"}", RestText/binary>> ->
+            {exit_object, RestText};
         <<"[", RestText/binary>> ->
             {enter_array, RestText};
         <<"]", RestText/binary>> ->
             {exit_array, RestText};
+        <<":", RestText/binary>> ->
+            {colon, RestText};
         <<",", RestText/binary>> ->
             {comma, RestText};
         <<"true", RestText/binary>> ->
