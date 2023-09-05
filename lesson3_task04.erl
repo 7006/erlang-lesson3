66c6c7f151a01d72454acd3fe50daad1eb87c76d
Replit user <unknown>
2023-09-05 19:29:38 +0000

    allow parse json to proplists and map

--- lesson3_task04.erl
+++ lesson3_task04.erl
@@ -1,6 +1,6 @@
 -module(lesson3_task04).
 
--export([decode/1]).
+-export([decode/1, decode/2]).
 
 -define(is_whitespace(C), (C =:= $\s orelse C =:= $\t orelse C =:= $\n)).
 
@@ -10,65 +10,75 @@
 %% має вміти працювати з map
 %% має вміти працювати з proplists
 decode(Text) ->
+    decode(Text, proplists).
+
+decode(Text, proplists) ->
+    decode(Text, lesson3_task04_object_handler_proplists);
+decode(Text, map) ->
+    decode(Text, lesson3_task04_object_handler_map);
+decode(Text, ObjectHandler) ->
     case get_token(Text) of
         {value, V, <<>>} ->
             V;
         {enter_object, RestText} ->
-            {Object, <<>>} = decode_object(RestText),
+            {Object, <<>>} = decode_object(RestText, ObjectHandler),
             Object;
         {enter_array, RestText} ->
-            {Array, <<>>} = decode_array(RestText),
+            {Array, <<>>} = decode_array(RestText, ObjectHandler),
             Array
     end.
 
 %% ----------------------------------------------------------------------------
 %% decode_object
 %% ----------------------------------------------------------------------------
-decode_object(Text) ->
-    decode_object(Text, [], no_key, no_value).
+decode_object(Text, ObjectHandler) ->
+    Object = ObjectHandler:new(),
+    decode_object(Text, Object, no_key, no_value, ObjectHandler).
 
-decode_object(Text, Object, Key, Value) ->
+decode_object(Text, Object, Key, Value, ObjectHandler) ->
     case get_token(Text) of
         {value, K, RestText} when Key =:= no_key, Value =:= no_value ->
-            decode_object(RestText, Object, K, no_value);
+            decode_object(RestText, Object, K, no_value, ObjectHandler);
         {colon, RestText} when Key =/= no_key ->
-            decode_object(RestText, Object, Key, no_value);
+            decode_object(RestText, Object, Key, no_value, ObjectHandler);
         {value, Val, RestText} when Key =/= no_key, Value =:= no_value ->
-            decode_object(RestText, Object, Key, Val);
+            decode_object(RestText, Object, Key, Val, ObjectHandler);
         {enter_array, RestText} when Key =/= no_key, Value =:= no_value ->
-            {Array, NextText} = decode_array(RestText),
-            decode_object(NextText, Object, Key, Array);
+            {Array, NextText} = decode_array(RestText, ObjectHandler),
+            decode_object(NextText, Object, Key, Array, ObjectHandler);
         {enter_object, RestText} when Key =/= no_key, Value =:= no_value ->
-            {NestedObject, NextText} = decode_object(RestText),
-            decode_object(NextText, Object, Key, NestedObject);
+            {NestedObject, NextText} = decode_object(RestText, ObjectHandler),
+            decode_object(NextText, Object, Key, NestedObject, ObjectHandler);
         {comma, RestText} when Key =/= no_key, Value =/= no_value ->
-            decode_object(RestText, [{Key, Value} | Object], no_key, no_value);
+            NextObject = ObjectHandler:put(Key, Value, Object),
+            decode_object(RestText, NextObject, no_key, no_value, ObjectHandler);
         {exit_object, RestText} when Key =:= no_key, Value =:= no_value ->
             {Object, RestText};
         {exit_object, RestText} when Key =/= no_key, Value =/= no_value ->
-            {lesson3_lists:reverse([{Key, Value} | Object]), RestText}
+            NextObject = ObjectHandler:done(Key, Value, Object),
+            {NextObject, RestText}
     end.
 
 %% ----------------------------------------------------------------------------
 %% decode_array
 %% ----------------------------------------------------------------------------
-decode_array(Text) ->
-    decode_array(Text, []).
+decode_array(Text, ObjectHandler) ->
+    decode_array(Text, [], ObjectHandler).
 
-decode_array(Text, Array) ->
+decode_array(Text, Array, ObjectHandler) ->
     case get_token(Text) of
         {exit_array, NextText} ->
             {lesson3_lists:reverse(Array), NextText};
         {comma, RestText} ->
-            decode_array(RestText, Array);
+            decode_array(RestText, Array, ObjectHandler);
         {value, Value, RestText} ->
-            decode_array(RestText, [Value | Array]);
+            decode_array(RestText, [Value | Array], ObjectHandler);
         {enter_array, RestText} ->
-            {NestedArray, NextText} = decode_array(RestText),
-            decode_array(NextText, [NestedArray | Array]);
+            {NestedArray, NextText} = decode_array(RestText, ObjectHandler),
+            decode_array(NextText, [NestedArray | Array], ObjectHandler);
         {enter_object, RestText} ->
-            {NestedObject, NextText} = decode_object(RestText),
-            decode_array(NextText, [NestedObject | Array])
+            {NestedObject, NextText} = decode_object(RestText, ObjectHandler),
+            decode_array(NextText, [NestedObject | Array], ObjectHandler)
     end.
 
 %% ----------------------------------------------------------------------------
@@ -76,7 +86,7 @@ decode_array(Text, Array) ->
 %% ----------------------------------------------------------------------------
 get_token(Text) ->
     case Text of
-        <<C, RestText/binary>> when ?is_whitespace(C) ->
+        <<ObjectHandler, RestText/binary>> when ?is_whitespace(ObjectHandler) ->
             get_token(RestText);
         <<"{", RestText/binary>> ->
             {enter_object, RestText};
---
+++ lesson3_task04_object_handler_map.erl
@@ -0,0 +1,18 @@
+-module(lesson3_task04_object_handler_map).
+
+-behaviour(lesson3_task04_object_handler).
+
+-export([
+    new/0,
+    put/3,
+    done/3
+]).
+
+new() ->
+    maps:new().
+
+put(Key, Value, Object) ->
+    maps:put(Key, Value, Object).
+
+done(Key, Value, Object) ->
+    put(Key, Value, Object).
